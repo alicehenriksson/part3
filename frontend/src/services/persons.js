5d12eee82d2decc9b9e63253e8965f42044bb06e
Alice <tommy.j.m.henriksson@gmail.com>
2022-12-27 11:35:59 +0200

    added api token

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,23 +1,40 @@
 import axios from 'axios'
 const baseUrl = 'https://www.backend-production-2130.up.railway.app/api/persons'
+const token = '67bff9cb-ede9-4fbe-99cd-ef341edd862e'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
+    const request = axios.get(baseUrl,{
+        headers: {
+            'Authorization': `Basic ${token}`
+         }
+    })
     return request.then(response => response.data)
 }
 
 const create = newObject => {
-    const request = axios.post(baseUrl,newObject)
+    const request = axios.post(baseUrl,newObject,{
+        headers: {
+            'Authorization': `Basic ${token}`
+         }
+    })
     return request.then(response => response.data)
 }
 
 const remove = objectId => {
-    const request = axios.delete(`${baseUrl}/${objectId}`)
+    const request = axios.delete(`${baseUrl}/${objectId}`,{
+        headers: {
+            'Authorization': `Basic ${token}`
+         }
+    })
     return request.then(response => response.data)
 }
 
 const update = (objectId,newData) => {
-    const request = axios.put(`${baseUrl}/${objectId}`,newData)
+    const request = axios.put(`${baseUrl}/${objectId}`,newData,{
+        headers: {
+            'Authorization': `Basic ${token}`
+         }
+    })
     return request.then(response => response.data)
 }
 
