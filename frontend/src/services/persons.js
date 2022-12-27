555b6bd89b7736549375a482788d155e2c82bb1a
Alice <tommy.j.m.henriksson@gmail.com>
2022-12-27 11:00:30 +0200

    added url for backend

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/persons'
+const baseUrl = 'https://www.backend-production-2130.up.railway.app/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
